db4b77a5906160481bb3e6d0e955fec09548e59b
Emilie Yeager <emilieyeager@gmail.com>
2015-08-20 21:53:20 -0600

    Fixed urls and added whoweare page

--- ecommerceproject/ecommerceproject/views.py
+++ ecommerceproject/ecommerceproject/views.py
@@ -4,18 +4,5 @@ from django.core.mail import send_mail
 
 def index(request):
 	return redirect('/home/')
-
-def contact(request):
-	return render(request, 'home/contact.html', {})
-
-def sent(request):
-	print 'sent'
-	name = request.POST['InputName']
-	email = request.POST['InputEmail']
-	message = request.POST['InputMessage']
-	subject = request.POST['InputSubject']
-	send_mail('Subject here', message, email,
-	['gmdavis1@gmail.com'], fail_silently=False)
-	return redirect('/contact')
    
 # info@abriendomen return redirect('/home')
--- ecommerceproject/home/urls.py
+++ ecommerceproject/home/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
  
 urlpatterns = [
- url(r'^$', views.index, name='index'),
+ url(r'^$', views.index, name='home'),
+ url(r'^whoweare$', views.whoweare, name='whoweare'),
+ url(r'^contact$', views.contact, name = 'contact'),
+ url(r'^whoweare', views.whoweare, name = 'whoweare'),
+ url(r'^sent', views.sent, name = 'sent'),
 ]
